816c27bb5c03c5640bcaad1e860c7ff8c5663b4e
incorrect-horse <vincenso_h@yahoo.com>
2023-03-31 13:38:22 -0400

    admin page, base template (html pages), nav bar

--- job_application/views.py
+++ job_application/views.py
@@ -9,11 +9,11 @@ def index(request):
     if request.method == "POST":
         form = ApplicationForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data["first_name"]
-            last_name = form.cleaned_data["last_name"]
+            first_name = form.cleaned_data["first_name"].title()
+            last_name = form.cleaned_data["last_name"].title()
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
-            occupation = form.cleaned_data["occupation"]
+            occupation = form.cleaned_data["occupation"].title()
 
             Form.objects.create(first_name=first_name, last_name=last_name,
                                 email=email, date=date, occupation=occupation)
@@ -32,3 +32,15 @@ def index(request):
 
             messages.success(request, "Form submmitted successfully!")
     return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def job_postings(request):
+    return render(request, "job_postings.html")
